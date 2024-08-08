4521cb469e154c5f7f92c545c3c85786a231cbd8
dredevs <contact.dredev@gmail.com>
2024-08-07 21:08:04 -0700

    nice

--- src/types/react.js
+++ src/types/react.js
@@ -2,7 +2,7 @@
 const e = React.createElement;
 
 function App() {
-  return e('h1', null, 'Hello, React!');
+  return e('h1', null, '');
 }
 
 ReactDOM.render(e(App), document.getElementById('react-root'));
